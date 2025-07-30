c125f0e2471cb0a15f63ce77178b86d1ba8bc3b5
Julia <juliacambrosini@gmail.com>
2025-07-30 01:35:21 -0300

    creating settings

--- meu_projeto/cadastros/models.py
+++ meu_projeto/cadastros/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.IntegerField(primary_key=True)
+    nome_usuario = models.CharField(max_length=255)
+    email = models.EmailField()
+    senha = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_usuario
+
+
+class CaoGuia(models.Model):
+    id_cao = models.IntegerField(primary_key=True)
+    nome_cao = models.CharField(max_length=255)
+    raca = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=10)
+    nascimento_cao = models.DateField()
+    peso_cao = models.FloatField()
+    tamanho = models.FloatField()
+    inicio_treinamento = models.DateField()
+    termino_treinamento = models.DateField()
+    total_horas_treinadas = models.TimeField()
+    treinador_responsavel = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome_cao
+
+
+class Candidato(models.Model):
+    class Considerado(models.TextChoices):
+        APTO = 'Apto', 'Apto'
+        INAPTO = 'Inapto', 'Inapto'
+
+    id_usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
+    nome_candidato = models.CharField(max_length=255)
+    nascimento_candidato = models.DateField()
+    altura = models.FloatField()
+    peso_candidato = models.FloatField()
+    estado_civil = models.CharField(max_length=100)
+    religiao = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=Considerado.choices)
+
+    def __str__(self):
+        return self.nome_candidato
+
+
+class FormacaoDupla(models.Model):
+    id_cao = models.ForeignKey(CaoGuia, null=True, on_delete=models.SET_NULL)
+    id_usuario = models.ForeignKey(Candidato, null=True, on_delete=models.SET_NULL)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
--- meu_projeto/meu_projeto/settings.py
+++ meu_projeto/meu_projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cadastros',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'meu_projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caes_guias',
+        'USER': 'postgres',
+        'PASSWORD': 'ferju123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
